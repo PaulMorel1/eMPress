89efc83b02aa6582c01c0199af965d3997d24099
Evan X. Merz <evanxmerz@yahoo.com>
2021-05-30 19:03:22 -0700

    Added alt text to the banner images. Added a comment about moving to modern image components

--- src/components/HeroHeader.js
+++ src/components/HeroHeader.js
@@ -7,6 +7,9 @@ import PageHeaderMenu from "./PageHeaderMenu.js";
 /**
  * See reference by Emma Bostian at 
  * https://itnext.io/reading-data-from-a-json-file-with-gatsby-graphql-572b18ab98a
+ * 
+ * TODO: I'd like this to use GatsbyImage or StaticImage without making the config
+ *       any more complex.
  */
 export default function HeroHeader() {
   const { site } = useStaticQuery(
@@ -25,8 +28,16 @@ export default function HeroHeader() {
 
   return (
     <header className={[HeroHeaderStyles.container].join(' ')}>
-      <img src={`/images/${site.siteMetadata.desktopHeroImage}`} className={HeroHeaderStyles.desktopHeroImage} />
-      <img src={`/images/${site.siteMetadata.mobileHeroImage}`} className={HeroHeaderStyles.mobileHeroImage} />
+      <img 
+        src={`/images/${site.siteMetadata.desktopHeroImage}`}
+        className={HeroHeaderStyles.desktopHeroImage}
+        alt={`Banner for ${site.siteMetadata.title}`}
+      />
+      <img
+        src={`/images/${site.siteMetadata.mobileHeroImage}`}
+        className={HeroHeaderStyles.mobileHeroImage}
+        alt={`Banner for ${site.siteMetadata.title}`}
+      />
       <div className={HeroHeaderStyles.titleContainer}>
         <Link to="/">
           <h1>{site.siteMetadata.title}</h1>
